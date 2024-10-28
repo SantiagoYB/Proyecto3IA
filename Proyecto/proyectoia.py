871132d340a65e4a1e8f64805c1b03f8d09eee44
SantiagoYB <98602870+SantiagoYB@users.noreply.github.com>
2024-10-27 23:34:49 -0500

    Se """"termina"""" el proyecto

--- Proyecto/proyectoia.py
+++ Proyecto/proyectoia.py
@@ -2,7 +2,7 @@
 # Lo del pyAgrum está basado en el video: https://www.youtube.com/watch?v=Cdee62uUWno
 #comando para correr  el codigo python -m Proyecto.proyectoia
 
-from Proyecto.red_bayesiana import RedBayesiana  # Importación absoluta
+from Proyecto.red_bayesiana import RedBayesiana  
 
 if __name__ == "__main__":
     red = RedBayesiana()
@@ -10,12 +10,10 @@ if __name__ == "__main__":
     red.cargar_tablas_probabilidad('Proyecto/tabla_probabilidad.txt')
     red.mostrar_tablas_probabilidad()
 
-    # Mostrar opciones de nodos para la consulta
     print("\nNodos disponibles para la consulta y evidencia:")
     for nombre_nodo in red.nodos:
         print(f" - {nombre_nodo}")
 
-    # Solicitar al usuario el nodo de consulta
     query = input("\nIntroduce el nombre del nodo para realizar la consulta: ")
     while query not in red.nodos:
         print("Nodo no válido. Selecciona uno de los nodos disponibles.")
@@ -23,15 +21,14 @@ if __name__ == "__main__":
 
     evidencia = {}
     
-    # Solicitar nodos de evidencia al usuario
     while True:
         nodo = input("\nIntroduce el nombre de un nodo de evidencia o escribe 'salir' para terminar: ")
         if nodo.lower() == 'salir':
             break
-        if nodo in red.nodos:  # Verificar que el nodo existe en la red
+        if nodo in red.nodos:  
             try:
                 estado = int(input(f"Introduce el estado de '{nodo}' (0 o 1): "))
-                if estado in [0, 1]:  # Asegurarse de que el estado sea válido
+                if estado in [0, 1]: 
                     evidencia[nodo] = estado
                 else:
                     print("Estado inválido. Solo se permiten los valores 0 o 1.")
@@ -40,6 +37,5 @@ if __name__ == "__main__":
         else:
             print(f"El nodo '{nodo}' no existe en la red. Intenta con otro.")
 
-    # Realizar inferencia sólo si el nodo de consulta existe en la red
     resultado = red.inferir_por_enumeracion(query, evidencia)
     print(f"\nProbabilidad de '{query}' dada la evidencia {evidencia}: {resultado}")
